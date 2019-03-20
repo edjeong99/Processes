b819ddb7a2326a47fb640b62b2e2e5c662fe454b
Edward J <edjeong99@gmail.com>
2019-03-20 17:28:09 -0400

    finish ex4

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -3,6 +3,9 @@
 // `execl()`, `execle()`, `execv()`, and others. Why do you think there 
 // are so many variants of the same basic call?
 
+//--> variants give options to users when using exec.
+//--> for example, arguments can be passed as list or array
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -10,7 +13,19 @@
 
 int main(void)
 {
-    // Your code here    
-
+  int rc = fork();
+  
+    if (rc < 0) {    // fork failed; exit
+   
+        exit(1);
+    } else if (rc == 0) {    // child process satisfies this branch
+         printf("Children\n");
+         execlp("ls", "ls", NULL);
+        perror("exec");
+         exit(1);   
+    } else {
+        int wc = waitpid(rc, NULL, 0);
+        printf("Parent\n");
+       }
     return 0;
 }
